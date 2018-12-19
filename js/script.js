7697c5ed6d5d1d79e99cdcc5960662f17d5bac68
aliceodli <44995631+aliceodli@users.noreply.github.com>
2018-12-19 21:38:51 +0300

    Правки в вёрстке

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-    var link = document.querySelector(".button-find");
-    var searchform = document.querySelector(".search-form");
-    var arrival = document.getElementById("arrival-date");
-    var departure = document.getElementById("departure");
-    var adults = document.getElementById("adults");
+var link = document.querySelector(".button-find");
+var searchform = document.querySelector(".search-form");
+var arrival = document.getElementById("arrival-date");
+var departure = document.getElementById("departure");
+var adults = document.getElementById("adults");
 
-    link.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      searchform.classList.toggle("form-show");
-      arrival.focus();
-    });
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  searchform.classList.toggle("form-show");
+  arrival.focus();
+});
 
-    searchform.addEventListener("submit", function (evt) {
-      if (!arrival.value || !departure.value || !adults.value) {
-        evt.preventDefault();
-        arrival.classList.add("form-error")
-        departure.classList.add("form-error")
-        adults.classList.add("form-error")
-      }
-    });
+searchform.addEventListener("submit", function (evt) {
+  if (!arrival.value || !departure.value || !adults.value) {
+    evt.preventDefault();
+    arrival.classList.add("form-error")
+    departure.classList.add("form-error")
+    adults.classList.add("form-error")
+  }
+});
